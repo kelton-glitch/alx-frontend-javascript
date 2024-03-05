6d3ffcda0438fccd391455c8951563b332177edc
Kamgo N Elton <kamgoelton89@gmail.com>
2024-03-05 05:51:51 +0000

    fix

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,14 +1,9 @@
 const getNeighborhoodsList = () => {
-  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const addNeighborhood = (newNeighborhood) => {
-    sanFranciscoNeighborhoods.push(newNeighborhood);
-    return sanFranciscoNeighborhoods;
-  };
-
-  return {
-    sanFranciscoNeighborhoods,
-    addNeighborhood
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
   };
 };
 
