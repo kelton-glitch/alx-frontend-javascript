7e584987f40d1fb4c8e72094e23f904c7f0a0e1a
Kamgo N Elton <kamgoelton89@gmail.com>
2024-03-06 12:24:49 +0100

    restructure

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,13 @@
-export default function getFullResponseFromAPI (success) {
-  return new Promise((resolve, reject) => {
-    // Simulating an API request with setTimeout
-    setTimeout(() => {
-      if (success) {
-        // Resolve the promise with a success response
-        resolve({ status: 200, body: 'Success' });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    }, 1000);
-  });
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        // Simulating an API request with setTimeout
+        setTimeout(() => {
+            if (success) {
+                // Resolve the promise with a success response
+                resolve({ status: 200, body: 'Success' });
+            } else {
+                reject(new Error('The fake API is not working currently'));
+            }
+        }, 1000);
+    });
 }
