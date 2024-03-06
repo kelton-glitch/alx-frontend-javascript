cb03ec51824ca1da8f5938641aeafc2e1a0ae59e
Kamgo N Elton <kamgoelton89@gmail.com>
2024-03-06 11:51:38 +0000

    fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,7 @@
-/**
- * @return {value} which represents the output of the data
- */
-export default function getResponsefromAPI() {
+export default function getResponsefromAPI () {
   return new Promise((resolve) => {
     setTimeout(() => {
-      const response = {data: 'true'};
+      const response = { data: 'true' };
       resolve(response);
     }, 1000);
   });
--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,8 @@
-export default function getFullResponseFromAPI (success) {
+function getFullResponseFromAPI (success) {
   return new Promise((resolve, reject) => {
-    // Simulating an API request with setTimeout
-    setTimeout(() => {
-      if (success) {
-        // Resolve the promise with a success response
-        resolve({ status: 200, body: 'Success' });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    }, 1000);
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(new Error('The fake API is not working currently'));
   });
 }
+
+export default getFullResponseFromAPI;
