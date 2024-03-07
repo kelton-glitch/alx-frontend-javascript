0c99c5630aa930dd9a8ac81b293ad8b249893377
Kamgo N Elton <kamgoelton89@gmail.com>
2024-03-07 16:04:15 +0000

    minor fixes

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -5,7 +5,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const fullBudget = {
     ...budget,
     getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuro: (income) => `${income} euros`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;
