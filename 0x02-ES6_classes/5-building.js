8bcc13721c9bd5b851856ee67df49d92d60acfd4
Kamgo N Elton <kamgoelton89@gmail.com>
2024-03-08 01:14:55 +0000

    fixes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extendung building must override evacuationWarningMessage');
+      throw new Error('Class extendung Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
