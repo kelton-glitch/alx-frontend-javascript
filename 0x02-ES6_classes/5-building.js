f88f8409817198f02cb1e6a2647bc541dde95fef
Kamgo N Elton <kamgoelton89@gmail.com>
2024-03-08 00:38:52 +0000

    eslinting

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,16 @@
 export default class Building {
-    constructor(sqft) {
-        if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-            throw new Error('Class extendung building must override evacuationWarningMessage');
-        }
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extendung building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    set sqft(sqft) {
-        this._sqft = sqft;
-    }
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
 }
