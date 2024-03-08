3078ed221fc6ea9c8b6a28b6e6bb545d2316c35b
Kamgo N Elton <kamgoelton89@gmail.com>
2024-03-08 00:52:39 +0000

    fixes

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -21,7 +21,7 @@ export class StudentHolberton {
   }
 
   get fullName() {
-    return `${this._firstName} ${this.lastName}`;
+    return `${this._firstName} ${this._lastName}`;
   }
 
   get holbertonClass() {
