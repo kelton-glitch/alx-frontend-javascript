77e653bd85479d76cd2d50745e1ce50f410240be
Kamgo N Elton <kamgoelton89@gmail.com>
2024-03-11 14:36:01 +0000

    tasks 5 to 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  const result = [];
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.substring(startString.length));
+    }
+  }
+  return result.join('-');
+}
